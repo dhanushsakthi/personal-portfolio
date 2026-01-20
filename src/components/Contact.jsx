2f2ea2266b0df7e6e3465a1666bc8016704a8c79
dhanushsakthi <dhanushsakthi@users.noreply.github.com>
2026-01-20 23:02:25 +0530

    fix: update instagram social link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     const socials = [
         { icon: <Linkedin size={24} />, href: "https://www.linkedin.com/in/dhanush-s-47830a2a1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", label: "LinkedIn" },
         { icon: <Github size={24} />, href: "https://github.com/dhanushsakthi/personal-portfolio", label: "GitHub" },
-        { icon: <Instagram size={24} />, href: "https://www.linkedin.com/in/dhanush-s-47830a2a1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", label: "Instagram" },
+        { icon: <Instagram size={24} />, href: "https://www.instagram.com/dhanushz.05_?utm_source=qr&igsh=dzdlc3d6YXNqdG1p", label: "Instagram" },
     ];
 
     return (
